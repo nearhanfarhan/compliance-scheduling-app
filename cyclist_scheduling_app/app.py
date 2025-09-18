6164a72c4e7adda44e742a815805c791b643d19e
Farhan <mohammed.f.chaudhry@gmail.com>
2025-09-18 15:08:38 +0100

    added ext.li.me

--- cyclist_scheduling_app/app.py
+++ cyclist_scheduling_app/app.py
@@ -74,7 +74,7 @@ else:
         user_email = user_info.get('email')
 
         # Check if the email domain is correct
-        if user_email and user_email.endswith('@li.me'):
+        if user_email and user_email.endswith(('@li.me', '@ext.li.me')):
             
             st.sidebar.success(f"Logged in as {user_info.get('name')}")
             
